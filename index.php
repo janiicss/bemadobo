592a4e0c503bbe5af44d2b637c8d295c5e11d0ca
janica chioco <janicachioco@janicas-MacBook-Pro.local>
2018-10-09 14:55:45 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chicken Cordon Bleu</title>
+
+    <link rel="stylesheet" href="./assets/css/dist/main.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Recipes</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="content">
+                <article class="recipe">
+                    <h1 class="recipe__title">Chicken Cordon Bleu</h1>
+                    <div class="recipe__img">
+                        <img src="./assets/img/cordon-bleu.jpg" alt="Chicken Cordon Bleu" />
+                        <figcaption>Chicken Cordon Bleu</figcaption>
+                    </div>
+                    <div class="recipe__caption">
+                        <p>Chicken Cordon Bleu is a dish composed of chicken breasts, ham, and Swiss cheese. The ham and Swiss cheese are arranged on top of the chicken breast and rolled together then dredged in bread crumbs.</p>
+                    </div>
+                    <hr />
+                    <div class="recipe__content">
+                        <div class="details">
+                            <ul class="details__prep">
+                                <li><strong>Prep Time  </strong> 10 minutes</li>
+                                <li><strong>Cook Time  </strong> 25 minutes</li>
+                                <li><strong>Total Time  </strong> 35 minutes</li>
+                                <li><strong>Servings </strong> 4</li>
+                            </ul>
+                            <h2>Ingredients</h2>
+                            <ul class="details__ingr">
+                                <li>4 pieces boneless chicken breast about 6 ounces each</li>
+                                <li>1/2 cup breadcrumbs</li>
+                                <li>12 slices thin sliced ham</li>
+                                <li>4 slices of Swiss cheese</li>
+                                <li>1 piece raw egg</li>
+                                <li>4 teaspoon butter softened</li>
+                                <li>1/4 teaspoon salt</li>
+                                <li>2 tsp paprika optional</li>
+                            </ul>
+                        </div>
+                        <div class="details">
+                            <h2>Instructions</h2>
+                            <ul class="details__instr">
+                                <li>Preheat oven to 350 degrees Fahrenheit</li>
+                                <li>Combine softened butter and salt then mix well</li>
+                                <li>Place a piece of chicken breast in a flat surface then brush the top with the butter-salt mixture and sprinkle some paprika.</li>
+                                <li>op the chicken breast with ham slices followed by the Swiss Cheese</li>
+                                <li>Start rolling the chicken breast from one end to the other. Make sure that it is tightly rolled. You may use a toothpick to fasten the end of the chicken breast to keep it from rolling back.</li>
+                                <li>After rolling all the chicken breast, combine the remaining butter-salt mixture with egg and whisk until every ingredient is evenly distributed.</li>
+                                <li>Brush the outer part of the chicken breasts with the egg-butter-salt mixture then dredge in bread crumbs.</li>
+                                <li>Place the chicken breasts in a greased baking dish then bake for 25 minutes. If the chicken is not fully cooked by this time, you can loosely cover it with aluminum foil and continue baking at lower temperature until the chicken is cooked.</li>
+                                <li>Remove from the oven and slice into individual servings.</li>
+                                <li>Serve hot. Share and enjoy!</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="tags">
+                        <span>Tags: </span>
+                        <span class="tags__list"></li><a href="#">Chicken</a></span>,
+                        <span class="tags__list"></li><a href="#">Recipe</a></span>,
+                        <span class="tags__list"></li><a href="#">Dish</a></span>
+                    </div>
+                </article>
+            </div> 
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="copyright">
+                &copy; 2018 Adobo Recipe Book. All Rights Reserved.
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
